25f9d17ff4ae5f2057af7a03f4a9bf03c717337f
Pliskin707 <85575336+Pliskin707@users.noreply.github.com>
2021-12-21 15:05:17 +0100

    Deep sleep option
    __added__
    • second build configuration for modified ESP01 which are deepSleep() capable (see https://randomnerdtutorials.com/esp8266-deep-sleep-with-arduino-ide/)
    
    __changed__
    • Transmission cycle time changed to one minute
    • Default (unmodified) ESP01 will not put the WiFi modem to sleep if not needed

--- ESP01_NowSender/src/main.cpp
+++ ESP01_NowSender/src/main.cpp
@@ -2,10 +2,31 @@
 #include <ESP8266WiFi.h>
 #include "esp_now_ext/esp_now_ext.hpp"
 
+#ifndef DEEPSLEEP
+#warning Using normal (unmodified) ESP01 configuration: no deep sleep
+#define SLEEPDURATION_MINUTES(x)   ((x) * 60ul * 1000ul)
+#else
+#warning Using deep sleep mode: all variables get reset every time the controller wakes up! (use EEPROM persistent data is required)
+#define SLEEPDURATION_MINUTES(x)   ((x) * 60ul * 1000000ul)
+#endif
+
+#define LEDPIN  LED_BUILTIN
+
 static const uint8_t receiverMac[6] = {0x50, 0x02, 0x91, 0xFC, 0xCB, 0x1C};    // \brief Receviers MAC address
 static uint32_t loopCounter = 0;
 
+static void transmit (void)
+{
+  digitalWrite(LEDPIN, false);
+  // send data
+  EspNowExt.send(receiverMac, &loopCounter, sizeof(loopCounter));
+  loopCounter++;
+  digitalWrite(LEDPIN, true);
+}
+
 void setup() {
+  pinMode(LEDPIN, OUTPUT);
+
   // WiFi
   WiFi.mode(WIFI_STA);
   // a connection to a "real" wifi network is not required
@@ -13,9 +34,18 @@ void setup() {
   // ESP-NOW
   EspNowExt.begin();
   EspNowExt.addPeer(receiverMac);
+  
+  #ifdef DEEPSLEEP
+  transmit();
+  yield();
+  ESP.deepSleep(SLEEPDURATION_MINUTES(1));
+  #else
+  WiFi.forceSleepBegin();
+  #endif
 }
 
 void loop() {
+  #ifndef DEEPSLEEP
   static uint32_t nextTransmit = 0;
   const uint32_t time = millis();
 
@@ -25,12 +55,14 @@ void loop() {
     if (!nextTransmit)
       nextTransmit = time;
 
-    nextTransmit += 1000;
-
-    // send data
-    EspNowExt.send(receiverMac, &loopCounter, sizeof(loopCounter));
-    loopCounter++;
+    nextTransmit += SLEEPDURATION_MINUTES(1);
+    WiFi.forceSleepWake();
+    transmit();
+    WiFi.forceSleepBegin();
   }
   
   yield();
+  #else
+  // this will never be called since the deepSleep() function resets the µC
+  #endif
 }
